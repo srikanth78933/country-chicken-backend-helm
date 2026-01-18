69979f9851ea5b5dfa985f60e1dc45a1963c9989
srikanth78933 <srikanth.b78933@gmail.com>
2026-01-18 14:28:36 +0530

    Update push-to-nexus.sh

--- scripts/push-to-nexus.sh
+++ scripts/push-to-nexus.sh
@@ -6,6 +6,6 @@ source scripts/env.sh
 
 PACKAGE=$(ls -t helm-packages/*.tgz | head -1)
 
-curl -u "$NEXUS_USER:$NEXUS_PASS" \
+curl -u "$NEXUS_CREDS_USR:$NEXUS_CREDS_PSW" \
   --upload-file "$PACKAGE" \
   "$NEXUS_HELM_REPO_URL/$(basename "$PACKAGE")"
