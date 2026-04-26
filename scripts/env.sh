2de454abf5f2bf420ba60aad6bfad668b0cf564e
srikanth78933 <srikanth.b78933@gmail.com>
2026-04-26 18:04:41 +0530

    Update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,6 +5,10 @@ APP_NAME="country-chicken-backend"
 CHART_PATH="charts/$APP_NAME"
 NAMESPACE="country-chicken"
 
-# Nexus Helm repo
+# Nexus Helm repo (for Helm CLI)
 NEXUS_HELM_REPO="nexus-helm"
 NEXUS_HELM_REPO_URL="${NEXUS_HELM_REPO_URL:-http://51.21.169.25:8081/repository/helm-releases/}"
+
+# 🔥 NEW (Required for proper upload via API)
+NEXUS_BASE_URL="http://51.21.169.25:8081"
+NEXUS_HELM_REPO_NAME="helm-releases"
