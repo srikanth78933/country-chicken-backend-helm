7580e23e49ad2358a3b5a6a9daa7eb7fa9bead7d
srikanth78933 <srikanth.b78933@gmail.com>
2026-04-26 20:32:44 +0530

    Update NEXUS_BASE_URL to new IP address

--- scripts/env.sh
+++ scripts/env.sh
@@ -10,5 +10,5 @@ NEXUS_HELM_REPO="nexus-helm"
 NEXUS_HELM_REPO_URL="${NEXUS_HELM_REPO_URL:-http://51.21.169.25:8081/repository/helm-releases/}"
 
 # 🔥 NEW (Required for proper upload via API)
-NEXUS_BASE_URL="http://51.21.169.25:8081"
+NEXUS_BASE_URL="http://54.87.4.226:8081"
 NEXUS_HELM_REPO_NAME="helm-releases"
