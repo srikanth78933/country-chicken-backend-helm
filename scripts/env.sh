6e254974162b7969a5aa571368eb4f3cd526c227
srikanth78933 <srikanth.b78933@gmail.com>
2026-05-01 14:33:44 +0530

    Update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -7,7 +7,7 @@ NAMESPACE="country-chicken"
 
 # Nexus Helm repo (for Helm CLI)
 NEXUS_HELM_REPO="nexus-helm"
-NEXUS_HELM_REPO_URL="${NEXUS_HELM_REPO_URL:-http://54.226.123.235:8081/repository/helm-releases/}"
+NEXUS_HELM_REPO_URL="${NEXUS_HELM_REPO_URL:-https://54.226.123.235/repository/helm-releases/}"
 
 # 🔥 NEW (Required for proper upload via API)
 NEXUS_BASE_URL="https://54.226.123.235"
